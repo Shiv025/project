f932bcacd99f12a895a8bf4ef71ef4bc60f554ed
Itachi <126654436+Shiv025@users.noreply.github.com>
2024-02-02 22:11:25 +0530

    1st commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
-// import {uuid} from 'uuid';
+const backendURL = 'http://localhost:4000';
+// import axios from "axios";
 
 const Home = () => {
 
@@ -10,54 +10,94 @@ const Home = () => {
 
 
   const [title, setTitle] = useState("");
-  const [file, setFile] = useState("");
+  const [file, setFile] = useState([]);
+  const [media, setMedia] = useState([]);
+
+
+  useEffect(() => {
+    fetchMedia();
+  }, [])
+
+
+  const fetchMedia = async () => {
+    try {
+      let result = await fetch(`${backendURL}/getMedia`)
+      result = await result.json();
+      setMedia(result?.data)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const deleteMedia = async (id) => {
+    try {
+      const payload = { mediaId: id }
+      let result = await fetch(`${backendURL}/deleteMedia`, {
+        method: "delete",
+        body: JSON.stringify(payload),
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      })
+      result = await result.json();
+      if (result?.status === true) fetchMedia();
+      // console.log(result?.message)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
   let isLogIn = JSON.parse(localStorage.getItem("isLoggedIn"))
   const [isLoggedIn, setisLoggedIn] = useState(isLogIn)
   const [currentUser, setCurrentUser] = useState(userData)
 
   useEffect(() => {
     if (!isLoggedIn) {
-      navigate('/login')
+      navigate('/login');
     }
   }, [isLoggedIn])
 
   const submitForm = async (e) => {
     e.preventDefault();         //to prevent app frm reloading
-    const formData = new FormData();
-    formData.append("title", title);
-    formData.append("file", file);
-    console.log(title, file);
-    const result = await fetch('http://localhost:4000/upload-file', {
-      method: 'post',
-     header: {"Content-type": "multipart/form-data"},
-     body: formData
-
-    })
-    // const result = await axios.post('http://localhost:4000/upload-file', formData,
-    // {
-    //   header: {"Content-type": "multipart/form-data"}
-    // })
-    console.log(result)
-    // let obj ={
-    //   title : title ,
-    //   file: file, 
-    //   id: uuid()
-    //   }
+    try {
+      const formData = new FormData();
+      formData.append("title", title);
+      formData.append("file", file);
+      let result = await fetch(`${backendURL}/uploads`, {
+        method: "post",
+        body: formData
+      })
+      result = await result.json();
+      if (result?.status === true) {
+        fetchMedia();
+        setFile([]);
+        setTitle("");
+      };
+      // console.log(result)
+    } catch (error) {
+      console.log(error)
+    }
   }
+
 
   return (
     <div className="text-white">
       <div className="flex flex-col items-center justify-center h-screen dark min-h-[100vh] bg-[#9F9F9F]">
         <form
-        id="formDetails"
+          action="/upload"
+          method="POST"
+          encType="multipart/form-data"
+          id="formDetails"
           className="w-2/5 bg-gray-800 rounded-lg shadow-md p-6 flex flex-col items-center justify-center"
           onSubmit={submitForm}
         >
           <h1 className="text-xl mb-4">Upload your files here 👇</h1>
           <input
-            className='className="bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150 w-full md:w-[90%]"'
+            className='className="bg-gray-700 text-gray-900 border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150 w-full md:w-[90%]"'
             type="text"
             placeholder="Title"
+            value={title}
             required
             onChange={(e) => setTitle(e.target.value)}
           />
@@ -65,7 +105,8 @@ const Home = () => {
           <input
             type="file"
             className='className="bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150 w-full md:w-[90%]"'
-            multiple
+            name="files"
+            accept=".pdf,.xls,.xlsx,.png,jpeg,.jpg"
             required
             onChange={(e) => setFile(e.target.files[0])} //here it will accept 1st file as we'hv given[0]
           />
@@ -76,6 +117,28 @@ const Home = () => {
             Upload
           </button>
         </form>
+
+        <div className="mt-[100px] flex items-center flex-wrap gap-8 shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]">
+
+          {media?.map((item, index) => {
+            return (
+              <div key={index} className="relative">
+                <div style={{ position: "absolute", right: '-10px', top: '-10px' }}
+                  onClick={() => { deleteMedia(item['_id']) }}
+                >
+                  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M10 0C15.53 0 20 4.47 20 10C20 15.53 15.53 20 10 20C4.47 20 0 15.53 0 10C0 4.47 4.47 0 10 0ZM13.59 5L10 8.59L6.41 5L5 6.41L8.59 10L5 13.59L6.41 15L10 11.41L13.59 15L15 13.59L11.41 10L15 6.41L13.59 5Z" fill="#F72C1D" />
+                  </svg>
+                </div>
+                <img src={`${backendURL}${item.mediaPath}`} alt={item.mediaName}
+                  className="w-[100px] h-[100px]" />
+              </div>
+            )
+          })}
+
+        </div>
+
+
       </div>
     </div>
   );
---
+++ src/pages/UploadedFiles.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function UploadedFiles(props) {
+  return (
+    <div>
+        <div>
+                <h1>Uploaded Files</h1>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default UploadedFiles
